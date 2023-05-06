6f780021372ff7878e6b893a5439c9ff00a5863a
lefdawyy <jehadlefdawy123@gmail.com>
2023-05-06 22:47:33 +0300

    Add cognito auth

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,9 +1,13 @@
 import {NgModule} from '@angular/core';
 import {BrowserModule} from '@angular/platform-browser';
-
 import {AppRoutingModule} from './app-routing.module';
 import {AppComponent} from './app.component';
 import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import { AmplifyAuthenticatorModule } from '@aws-amplify/ui-angular';
+import { environment } from 'src/environments/environment.prod';
+import { Amplify } from 'aws-amplify';
+
+Amplify.configure(environment.cognito);
 
 @NgModule({
   declarations: [
@@ -12,6 +16,7 @@ import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
   imports: [
     BrowserModule,
     AppRoutingModule,
+    AmplifyAuthenticatorModule,
     BrowserAnimationsModule,
   ],
   providers: [],
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,4 +1,8 @@
 export const environment = {
   production: false,
-  apiBaseUrl: 'https://mze32ysg05.execute-api.us-east-1.amazonaws.com/'
+  apiBaseUrl: 'https://mze32ysg05.execute-api.us-east-1.amazonaws.com/',
+  cognito: {
+    userPoolId: 'eu-south-1_aqytra1zC',
+    userPoolWebClientId: '4m18og6qujenoctc5l5fjqm98o',
+  }
 };
